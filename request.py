2b8464ba7bf39df75ed78396f32d2c13d0c97d87
Emilio Rivera <192448@iberopuebla.mx>
2023-03-28 01:28:21 -0600

    Test script

--- request.py
+++ request.py
@@ -1,3 +1,5 @@
+# *script to test and debug the API
+
 from requests import post, delete
 
 #post_data = {'title':'Resident Evil Village','description':'Set a few years after the horrifying events in the critically acclaimed Resident Evil 7 biohazard, the all-new storyline begins with Ethan Winters and his wife Mia living peacefully in a new location, free from their past nightmares.\nJust as they are building their new life together, tragedy befalls them once again.\nWhen BSAA captain Chris Redfield attacks their home,\nEthan must once again head into hell to get his kidnapped daughter back.','developer':'CAPCOM Co., Ltd.','release_year':'2021-05-06','clasification':'C','image':'https://image.api.playstation.com/vulcan/ap/rnd/202101/0812/FkzwjnJknkrFlozkTdeQBMub.png'}
