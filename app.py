605f89b7410d53d3b087a78a7ff280c3ab8ec273
Emilio Rivera <192448@iberopuebla.mx>
2023-03-27 18:56:33 -0600

    Changes

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ def get_all_videogames():
     
     with Session(engine) as session:
         if request.method == 'GET':
+            #dictionary to store serialized data
+            
+
             all_videogames = session.execute(text('SELECT * FROM public.videogames'))
             print(all_videogames.all())
             return jsonify(all_videogames.all())
@@ -56,79 +59,3 @@ def get_dev_videogames(dev_name):
 if __name__ == '__main__':
     #run Flask object with debug output on terminal
     app.run(debug=True,port=5000)
-
-
-
-
-
-
-
-
-
-
-#get request to view the tasks
-@app.route((URI + 'tasks'), methods=['GET'])
-def get_tasks():
-    """ Returns the tasks data structure as a json """
-    return jsonify({'tasks':tasks})
-
-#get request to view specific task
-@app.route((URI + 'tasks/<int:id>'), methods=['GET'])
-def get_specific_task(id):
-    """ Look for the specified task on tasks data structure and return it as json if found """
-    #iterate through each task to look for the specified one
-    for n in tasks:
-        if n['id'] == id:
-            #if task is found, return it as a json
-            return jsonify({'task':n})
-    #if the task does not exists, 404 HTTP error code is returned
-    return abort(404)
-
-#post method to append a task to tasks data structure
-@app.route((URI + 'append'), methods=['POST'])
-def append_task():
-    """ append to tasks the specified task through the received HTTP packet """
-    #initialize the necessary variables with its respective value from the variables from the HTTP packet
-    post_name = request.form.get('name')
-    #check if post_name value is not null
-    if post_name == "":
-        return jsonify({'tasks':tasks})
-    #append the task
-    tasks.append({'id':(len(tasks) + 1),'name':post_name,'estado':False})
-    #return tasks
-    return jsonify({'tasks':tasks})
-
-#delete method to remove the specified task
-@app.route((URI + 'delete/<int:delete_id>'), methods=['DELETE'])
-def delete_task(delete_id):
-    """ remove the specified task through the URI from tasks data structure """
-    #iterate through tasks looking for the indicated task to be removed
-    for it in range(len(tasks)):
-        #if found, delete the indicated task and return tasks as json
-        if tasks[it]['id'] == delete_id:
-            del tasks[it]
-            return jsonify({'tasks':tasks})
-            break
-    #if task is not found, generate 404 HTTP error code
-    return abort(404)
-
-#put method to update a specified task
-@app.route((URI + 'update'), methods=['PUT'])
-def update_task():
-    """ update the specified task with the values of the HTTP packet data """
-    #initialize the id variable with the received value in the HTTP packet
-    put_id = int(request.form.get('id'))
-    #iterate through tasks looking for the indicated task to be updated
-    for it in tasks:
-        #if found, update the task and return tasks as json
-        if it['id'] == put_id:
-            it['estado'] = True
-            return jsonify({'tasks':tasks})
-    #if task does not exists, generate 404 HTTP error code
-    return abort(404)
-
-#API program start point ----->
-#check if current module is running as main
-if __name__ == '__main__':
-    #run Flask object with debug output on terminal
-    app.run(debug=True,port=5001)
