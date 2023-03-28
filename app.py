be14be527385e3b3987d27cd8446168a732f4811
Emilio Rivera <192448@iberopuebla.mx>
2023-03-27 22:05:16 -0600

    main GET implemented

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """-----------------------------------------------------------------------------------------------------------------------------------
-- Software Name: 
+- Software Name: Videogame API
 - Language: Python
 - Developers: Emilio Rivera Macías and Carlos Cancino Escobar
 - Date: March 27, 2023
@@ -13,17 +13,25 @@ from sqlalchemy.orm import Session
 import psycopg2
 
 #object declaration ----->
-
 #URI of the API
 URI = '/api/videogames/'
 
 #data base engine to establishing connection
-engine = create_engine('postgresql://emilio:C4iIa5Wr5BngpdmjpUwoF3BicwJX6ZAw@dpg-cgeusjpmbg568r4g3plg-a.oregon-postgres.render.com/examen2',echo=True)
+engine = create_engine('postgresql://emilio:C4iIa5Wr5BngpdmjpUwoF3BicwJX6ZAw@dpg-cgeusjpmbg568r4g3plg-a.oregon-postgres.render.com/examen2')
 
 #creating the Flask object app
 app = Flask(__name__)
 
 #function definition ----->
+#serialize data function
+def serialize_raw_query_data(raw_data):
+    """ serialize raw data retreived from the data base to send it as a json. Raw data is returned as a list of tuples """
+    serialized_data = []
+    #iterate through each tuple to serialize it in a list of dictionaries
+    for _tuple in raw_data:
+        serialized_data.append({'id':_tuple.videogame_id,'title':_tuple.title,'description':_tuple.description,'developer':_tuple.developer,'release_year':_tuple.release_year,'clasification':_tuple.clasification,'image':_tuple.image})
+    return jsonify(serialized_data)
+
 #GET request to view all videogames from the Data Base, POST request to append new videogames to the Data Base 
 #and DELETE request to remove the specified videogame from the Data Base
 @app.route(URI, methods=['GET','POST','DELETE'])
@@ -33,11 +41,7 @@ def get_all_videogames():
     with Session(engine) as session:
         if request.method == 'GET':
             #dictionary to store serialized data
-            
-
-            all_videogames = session.execute(text('SELECT * FROM public.videogames'))
-            print(all_videogames.all())
-            return jsonify(all_videogames.all())
+            return serialize_raw_query_data(session.execute(text('SELECT * FROM public.videogames')))
         elif request.method == 'POST':
             pass
         elif request.method == 'DELETE':
